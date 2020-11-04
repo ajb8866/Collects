7fe775ff64ba3ffeb87e8cc2406a48a39d5ea3ae
Averi Bates <Averi.J.Bates@ou.edu>
2020-11-04 16:51:46 -0600

    4

--- src/Collect.java
+++ src/Collect.java
@@ -1,18 +1,25 @@
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Set;
 import java.util.TreeMap;
 
 public class Collect {
 	HashMap<Integer, String> employeeMap = new HashMap<Integer, String>();
 	TreeMap<Integer, String> employeeTreeMap = new TreeMap<Integer, String>();
-	Set<String> employeeNameSet = new HashSet<String>(); 
+	LinkedList<String> deparment = new LinkedList<String>();
+	Set<String> employeeDeparment = new HashSet<String>(); 
 
 	
-	public Collect (HashMap<Integer, String> employeeMap)
+	public Collect (HashMap<Integer, String> employeeMap,LinkedList<String> deparment3)
 	{
 		this.employeeMap = employeeMap;
 		employeeTreeMap.putAll(employeeMap);
+		this.deparment = deparment3;
+		for(int x = 0; x < deparment3.size(); ++x)
+		{
+			employeeDeparment.add(deparment3.get(x));
+		}
 	
 		
 	}
@@ -24,6 +31,17 @@ public class Collect {
 	{
 		employeeMap.putIfAbsent(ID, name);
 		employeeTreeMap.putIfAbsent(ID, name);
+	}
+	
+	public void addToLinkedList(String name )
+	{
+		deparment.add(name);
+		employeeDeparment.add(name);
+	}
+	public boolean containsName(String key)
+	{
+		
+		return employeeDeparment.contains(key);
 	}
 	
 	//Show how you would use Set to find the unique elements in a List.
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,27 @@
 import java.util.HashMap;
+import java.util.LinkedList;
 
 public class Main {
 
 	public static void main(String[] args) {
-		HashMap<String, Integer> employeeMap = new HashMap<String, Integer>();
-		Collect company1 = new Collect(employeeMap);
+		HashMap<Integer, String> employeeMap = new HashMap<Integer, String>();
+		LinkedList<String> deparment = new LinkedList<String>();
+
+		employeeMap.put(10002,"Paul Rugh");
+		employeeMap.put(10042,"Mary Rugh");
+		employeeMap.put(14042,"Mary Lone");
+		employeeMap.put(14445,"Susan Thyme");
+		employeeMap.put(64555,"Kevin Baker");
+		
+		deparment.add("Marketing");
+		deparment.add("Accounting");
+		deparment.add("Accounting");
+		deparment.add("Sales");
+		deparment.add("HR");
+
+		
+		Collect company1 = new Collect(employeeMap, deparment);
+	
 		System.out.println(company1.getHashMap());
 
 	}
