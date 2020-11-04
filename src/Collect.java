20a22ec5f4fe4a5e7746d46121d12c4e08d9a4d1
Averi Bates <Averi.J.Bates@ou.edu>
2020-11-04 16:33:00 -0600

    3

--- src/Collect.java
+++ src/Collect.java
@@ -1,20 +1,41 @@
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeMap;
 
 public class Collect {
-	HashMap<String, Integer> employeeMap = new HashMap<String, Integer>();
+	HashMap<Integer, String> employeeMap = new HashMap<Integer, String>();
+	TreeMap<Integer, String> employeeTreeMap = new TreeMap<Integer, String>();
+	Set<String> employeeNameSet = new HashSet<String>(); 
+
 	
-	public Collect (HashMap<String, Integer> employeeMap)
+	public Collect (HashMap<Integer, String> employeeMap)
 	{
 		this.employeeMap = employeeMap;
+		employeeTreeMap.putAll(employeeMap);
+	
+		
 	}
-	public HashMap<String, Integer> getHashMap()
+	public HashMap<Integer, String> getHashMap()
 	{
 		return employeeMap;
 	}
 	public void addToHashMap(String name, int ID )
 	{
-		
+		employeeMap.putIfAbsent(ID, name);
+		employeeTreeMap.putIfAbsent(ID, name);
 	}
+	
+	//Show how you would use Set to find the unique elements in a List.
+	
+	
+	//Show an example of HashMap and a TreeMap, and explain the difference
+	//The difference between a HashMap and a TreeMap is that a TreeMap is sorted because of its inheritance from SortedMap
+	
+	//Which should you use (HashMap or TreeMap), if you have to guarantee the order of key, value pairs?
+	//You would use a treeMap because a hashMap is never in order, and always random
+	
+	
 	
 	
 
